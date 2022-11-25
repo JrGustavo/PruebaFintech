2c11935c1abf3fd5f0deb18ab87be4d21e079198
JrGustavo <gus@soypetal.com>
2022-11-25 12:07:51 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module FintechGo
+
+go 1.19
+
+require golang.org/x/crypto v0.3.0
---
+++ migrations/migrations.go
@@ -0,0 +1,58 @@
+package migrations
+
+import (
+	"FintechGo/helpers"
+	_ "github.com/jinzhu/gorm/dialects/postgres
+	_ "gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string
+	Email    string
+	Password string
+}
+
+type Account struct {
+	gorm.Model
+	Type    string
+	Name    string
+	Balance uint
+	UserID  uint
+}
+
+func connectDB() *gorm.DB {
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=user dbname=dbname password=password sslmode=disable")
+	helpers.HandleErr(err)
+	return db
+}
+
+func createAccounts() {
+	db := connectDB()
+
+	users := [2]User{
+		{Username: "Martin", Email: "martin@martin.com"},
+		{Username: "Michael", Email: "michael@michael.com"},
+	}
+
+	for i := 0; i < len(users); i++ {
+		// Correct one way
+		// generatedPassword := helpers.HashAndSalt([]byte(users[i].Username))
+		generatedPassword := helpers.HashOnlyVulnerable([]byte(users[i].Username))
+		user := User{Username: users[i].Username, Email: users[i].Email, Password: generatedPassword}
+		db.Create(&user)
+
+		account := Account{Type: "Daily Account", Name: string(users[i].Username + "'s" + " account"), Balance: uint(10000 * int(i+1)), UserID: user.ID}
+		db.Create(&account)
+	}
+	defer db.Close()
+}
+
+func Migrate() {
+	db := connectDB()
+	db.AutoMigrate(&User{}, &Account{})
+	defer db.Close()
+
+	createAccounts()
+}
